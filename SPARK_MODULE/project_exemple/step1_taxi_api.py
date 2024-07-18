a082c89502914388948df71354e20691a84617da
benjilabananee <fassi.benjamin@gmail.com>
2024-07-18 09:27:47 +0000

    project_exemple

--- SPARK_MODULE/project_exemple/step1_taxi_api.py
+++ SPARK_MODULE/project_exemple/step1_taxi_api.py
@@ -15,7 +15,7 @@ while True:
         producer = KafkaProducer(bootstrap_servers="course-kafka:9092")
         producer.send(topic="my_trip", value=json.dumps(row).encode('utf-8'))
     print(datetime.fromtimestamp(time.time()))
-    time.sleep(4)
+    time.sleep(1)
 
 
 
